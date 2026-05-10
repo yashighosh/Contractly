bb6de93467c31dac2ee5169b7a9f10082d53572e
Anik <160725845+coderanik@users.noreply.github.com>
2026-05-10 22:31:53 +0530

    chore: remove deprecated file ../backend/src/main/java/com/contractly/common/exception/UnauthorizedException.java

--- backend/src/main/java/com/contractly/common/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.contractly.common.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
